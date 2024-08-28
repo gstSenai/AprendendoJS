ddad1f7a0f22e5cda914f1951601d1887cb2295f
GUSTAVO DE SOUZA <gustavo_souza12@estudante.sesisenai.org.br>
2024-08-28 20:16:18 -0300

    s

--- SECAO-6-JS/js/main.js
+++ SECAO-6-JS/js/main.js
@@ -1,131 +1,131 @@
-// // 1 - métodos
+// 1 - métodos
 
-// // const pessoaMetodos = {
-// //     name : "Gustavo",
-// //     age : function(){
-// //         console.log(19);
-// //     }
-// // }
-
-// // console.log(pessoaMetodos.name);
-
-// // pessoaMetodos.age();
-
-// // 2 - aprofundando metodos
-
-// // const pessoa = {
-// //     nome : "Gustavo",
-
-// //     getNome:function() {
-// //         return this.nome;
-// //     },
-
-// //     setNome: function(novoNome) {
-// //         this.nome = novoNome;
-// //     },
-// // };
-
-// // console.log(pessoa.nome);
-
-// // console.log(pessoa.getNome());
-
-// // pessoa.setNome("Joaquim")
-
-// // console.log(pessoa.getNome());
-
-// // // 3 - prototype
-// // const text = "asd";
-
-// // console.log(Object.getPrototypeOf(text));
-
-// // const bool = true;
-
-// // console.log(Object.getPrototypeOf(bool));
-
-// // const arr = [];
-
-// // console.log(Object.getPrototypeOf(arr));
-
-// // // 4 - mais sobre prototype
-
-// // const myObject = {
-// //     a: "b"
-// // }
-
-// // console.log(Object.getPrototypeOf(myObject));
-
-// // console.log(Object.getPrototypeOf(myObject) === Object.prototype);
-
-// // const mySecondObject = Object.create(myObject)
-
-// // console.log(mySecondObject);
-
-// // console.log(mySecondObject.a);
-
-// // console.log(Object.getPrototypeOf(mySecondObject) === myObject);
-
-// // 5 - classe basicas
-
-// // const cachorro = {
-// //     raca: null,
-// //     patas: 4
-// // };
-
-// // const pastorAlemao = Object.create(cachorro)
-
-// // pastorAlemao.raca = "Pastor Alemão";
-
-// // console.log(pastorAlemao);
-
-// // console.log(pastorAlemao.patas);
-
-// // const bulldog = Object.create(cachorro)
-
-// // bulldog.raca = "Bulldog";
-
-// // 6 - função como classe - função construtora
-
-// // function criarCachorro(nome,raca){
-// //     const cachorro = Object.create({})
-
-// //     cachorro.nome = nome
-// //     cachorro.raca = raca
-
-// //     return cachorro
-// // }
-
-// // const bob = criarCachorro("Bob","Vira Lata");
-
-// // console.log(bob);
-
-// // 7 - funções como classe
-
-// function Cachorro(nome, raca) {
-//     this.nome = nome
-//     this.raca = raca
-// }
-
-// const husky = new Cachorro("Ozzy", "Husky")
-
-// console.log(husky);
-
-// // 8 - metodos na funcao construtora
-// Cachorro.prototype.uivar = function(){
-//     console.log("auuuuuuu");
-// }
-
-// console.log(Cachorro.prototype);
-
-// husky.uivar();
-
-// // 9 - classes es6(Melhor Jeito para criar Objeto)
-
-// class CachorroClasse{
-//     constructor(nome,raca){
-//         this.nome = nome,
-//         this.raca = raca
+// const pessoaMetodos = {
+//     name : "Gustavo",
+//     age : function(){
+//         console.log(19);
 //     }
 // }
+
+// console.log(pessoaMetodos.name);
+
+// pessoaMetodos.age();
+
+// 2 - aprofundando metodos
+
+// const pessoa = {
+//     nome : "Gustavo",
+
+//     getNome:function() {
+//         return this.nome;
+//     },
+
+//     setNome: function(novoNome) {
+//         this.nome = novoNome;
+//     },
+// };
+
+// console.log(pessoa.nome);
+
+// console.log(pessoa.getNome());
+
+// pessoa.setNome("Joaquim")
+
+// console.log(pessoa.getNome());
+
+// // 3 - prototype
+// const text = "asd";
+
+// console.log(Object.getPrototypeOf(text));
+
+// const bool = true;
+
+// console.log(Object.getPrototypeOf(bool));
+
+// const arr = [];
+
+// console.log(Object.getPrototypeOf(arr));
+
+// // 4 - mais sobre prototype
+
+// const myObject = {
+//     a: "b"
+// }
+
+// console.log(Object.getPrototypeOf(myObject));
+
+// console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+// const mySecondObject = Object.create(myObject)
+
+// console.log(mySecondObject);
+
+// console.log(mySecondObject.a);
+
+// console.log(Object.getPrototypeOf(mySecondObject) === myObject);
+
+// 5 - classe basicas
+
+// const cachorro = {
+//     raca: null,
+//     patas: 4
+// };
+
+// const pastorAlemao = Object.create(cachorro)
+
+// pastorAlemao.raca = "Pastor Alemão";
+
+// console.log(pastorAlemao);
+
+// console.log(pastorAlemao.patas);
+
+// const bulldog = Object.create(cachorro)
+
+// bulldog.raca = "Bulldog";
+
+// 6 - função como classe - função construtora
+
+// function criarCachorro(nome,raca){
+//     const cachorro = Object.create({})
+
+//     cachorro.nome = nome
+//     cachorro.raca = raca
+
+//     return cachorro
+// }
+
+// const bob = criarCachorro("Bob","Vira Lata");
+
+// console.log(bob);
+
+// 7 - funções como classe
+
+function Cachorro(nome, raca) {
+    this.nome = nome
+    this.raca = raca
+}
+
+const husky = new Cachorro("Ozzy", "Husky")
+
+console.log(husky);
+
+// 8 - metodos na funcao construtora
+Cachorro.prototype.uivar = function(){
+    console.log("auuuuuuu");
+}
+
+console.log(Cachorro.prototype);
+
+husky.uivar();
+
+// 9 - classes es6(Melhor Jeito para criar Objeto)
+
+class CachorroClasse{
+    constructor(nome,raca){
+        this.nome = nome,
+        this.raca = raca
+    }
+}
 
 // const jeff = new CachorroClasse("Jeff", "Labrador");
 
@@ -180,7 +180,7 @@
 // 12 Symbol 
 
 class Aviao {
-    constructor(marca, turbinas){
+    constructor(marca, turbinas) {
         this.marca = marca
         this.turbinas = turbinas
     }
@@ -197,7 +197,55 @@ console.log(boeing);
 console.log(boeing[asas]);
 
 // 13 - getter e setter
-class Post{
-    constructor(titulo,)
+class Post {
+    constructor(titulo, descricao, tags) {
+        this.titulo = titulo
+        this.descricao = descricao
+        this.tags = tags
+    }
+    get exibirTitulo() {
+        return `Você está lendo: ${this.titulo}`
+    }
 
+    set adicionarTag(tags){
+        const tagsArray = tags.split(", ");
+        this.tags = tagsArray
+    }
 }
+
+const myPost = new Post("Algum, Post", "É um post sobre programação");
+
+console.log(myPost.exibirTitulo);
+
+myPost.adicionarTag = "programação, javascripts, js"
+
+console.log(myPost);
+
+// 14 - Herança
+
+class Mamifero{
+    constructor(patas){
+        this.patas = patas
+    }
+}
+
+class Lobo extends Mamifero {
+    constructor(patas, nome){
+        super(patas, patas)
+        this.nome = nome
+    }
+}
+
+const shark = new Lobo(4,"gst");
+
+console.log(shark);
+
+// 15 - instanceoff
+
+console.log(shark instanceof Lobo);
+
+console.log(Lobo instanceof Mamifero);
+
+console.log(new Lobo(4, "teste")instanceof Mamifero);
+
+console.log(new Post("a", "b")instanceof Lobo);
---
+++ SECAO-7-JS/js/main.js
@@ -0,0 +1,58 @@
+// 1 - strict
+"use strict";
+
+//opa = "teste";
+
+const opa = "teste";
+
+//const undefined = "teste2";
+
+// delete [].length
+
+// 2 - console.log
+
+let a = 1
+let b = 2
+
+if (a === 1) {
+    a = b + 2;
+}
+
+console.log(a);
+
+for (let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
+
+if (a > 5) {
+    a = 25;
+}
+
+console.log(a);
+
+// 3 - debugger
+let c = 1
+let d = 2
+
+if (c == 1) {
+    c = d + 2;
+}
+
+//debugger;
+
+for (let i = 0; i < d; i++) {
+    d = c + 2;
+}
+
+console.log("executou o loop");
+
+//debugger;
+
+if (c > 5) {
+    c = 25;
+}
+
+// tratamento de dados
+
+
