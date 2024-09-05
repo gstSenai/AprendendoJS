c07552f9899f6029a24f38ae84ea64d7e5ca5255
GUSTAVO DE SOUZA <gustavo_souza12@estudante.sesisenai.org.br>
2024-09-05 16:16:31 -0300

    save

--- SECAO-4-JS/js/main.js
+++ SECAO-4-JS/js/main.js
@@ -1,142 +1,142 @@
 // // 1 - criando uma função 
-// function minhaFuncao() {
-//     console.log("Testando");
-// }
+function minhaFuncao() {
+    console.log("Testando");
+}
 
-// minhaFuncao()
+minhaFuncao()
 
-// const minhaFuncaoEmVariavel = function () {
-//     console.log("Função em variável");
-// };
+const minhaFuncaoEmVariavel = function () {
+    console.log("Função em variável");
+};
 
-// minhaFuncaoEmVariavel()
+minhaFuncaoEmVariavel()
 
-// function funcaoComParametro(txt) {
-//     console.log(`Imprimindo: ${txt}`);
-// }
+function funcaoComParametro(txt) {
+    console.log(`Imprimindo: ${txt}`);
+}
 
-// funcaoComParametro("Imprimindo alguma coisa");
+funcaoComParametro("Imprimindo alguma coisa");
 
-// funcaoComParametro("outra função");
+funcaoComParametro("outra função");
 
-// // 2 - return
+// 2 - return
 
-// const a = 10
-// const b = 20
-// const c = 30
+const a = 10
+const b = 20
+const c = 30
 
-// function soma(n1, n2) {
-//     return n1 + n2;
-// }
+function soma(n1, n2) {
+    return n1 + n2;
+}
 
-// const resultado = soma(a,b);
+const resultado = soma(a,b);
 
-// console.log(resultado);
+console.log(resultado);
 
-// // 3 - escopo da função 
+// 3 - escopo da função 
 
-// let y = 10;
+let y = 10;
 
-// function testandoEscopo() {
-//     let y = 20;
-//     console.log(`Y dentro da função é: ${y}`);
-// }
+function testandoEscopo() {
+    let y = 20;
+    console.log(`Y dentro da função é: ${y}`);
+}
 
-// testandoEscopo();
+testandoEscopo();
 
-// y = 25;
+y = 25;
 
-// console.log(`Y fora da função é: ${y}`);
+console.log(`Y fora da função é: ${y}`);
 
-// testandoEscopo();
+testandoEscopo();
 
-// // 4 - escopo aninhado
+// 4 - escopo aninhado
 
-// let m = 10;
+let m = 10;
 
-// function escopoAninhado() {
-//     let m = 20
+function escopoAninhado() {
+    let m = 20
 
-//     if(true){
-//         let m = 30;
+    if(true){
+        let m = 30;
 
         
-//         if(true){
-//             let m = 40;
+        if(true){
+            let m = 40;
 
-//             console.log(m);
-//         }
-//         console.log(m);
-//     }
-//     console.log(m);
-// }
+            console.log(m);
+        }
+        console.log(m);
+    }
+    console.log(m);
+}
 
-// escopoAninhado();
+escopoAninhado();
 
-// console.log(m);
+console.log(m);
 
-// // 5 - arrow function
-// const testeArrow = () => {
-//     console.log("Esta é uma arrow function");
-// };
+// 5 - arrow function
+const testeArrow = () => {
+    console.log("Esta é uma arrow function");
+};
 
-// testeArrow();
+testeArrow();
 
-// const parOuImpar = (n) => {
-//     if(n%2 === 0){
-//         console.log("Par");
-//         return;
-//     }
+const parOuImpar = (n) => {
+    if(n%2 === 0){
+        console.log("Par");
+        return;
+    }
 
-//     console.log("Impar");
-// }
+    console.log("Impar");
+}
 
-// parOuImpar(5)
+parOuImpar(5)
 
-// parOuImpar(10)
+parOuImpar(10)
 
-// // 6 - mais sobre arrow function
+// 6 - mais sobre arrow function
 
-// const raizQuadrada = (x) => {
-//     return x * x;
-// }
+const raizQuadrada = (x) => {
+    return x * x;
+}
 
-// console.log(raizQuadrada(4));
+console.log(raizQuadrada(4));
 
-// const raizQuadrada2 = (x) => x * x;
+const raizQuadrada2 = (x) => x * x;
 
-// console.log(raizQuadrada2(6));
+console.log(raizQuadrada2(6));
 
-// const helloWorld = () => console.log("Hello World");
+const helloWorld = () => console.log("Hello World");
 
-// helloWorld();
+helloWorld();
 
-// // 7 - parametro opcional
+// 7 - parametro opcional
 
-// const multiplication = function(m,n){
-//     if(n === undefined){
-//         return m * 2;
-//     }else{
-//         return m * n;
-//     }
-// };
+const multiplication = function(m,n){
+    if(n === undefined){
+        return m * 2;
+    }else{
+        return m * n;
+    }
+};
 
-// console.log(multiplication(5));
+console.log(multiplication(5));
 
-// console.log(multiplication(2,4));
+console.log(multiplication(2,4));
 
-// const greeting = (name) => {
+const greeting = (name) => {
 
-//     if(!name){
-//         console.log("olá");
-//         return
-//     }
-//     console.log(`Olá ${name}!`)
-// }
+    if(!name){
+        console.log("olá");
+        return
+    }
+    console.log(`Olá ${name}!`)
+}
 
-// greeting()
+greeting()
 
-// greeting("Matheus")
+greeting("Matheus")
 
 // 8 - valor default
 
--- SECAO-9-JS/js/scripts.js
+++ SECAO-9-JS/js/scripts.js
@@ -1,27 +1,27 @@
 // 1 - movendo pelo DOM
 
-console.log(document.body);
+// console.log(document.body);
 
-console.log(document.body.childNodes[1].childNodes);
+// console.log(document.body.childNodes[1].childNodes);
 
-console.log(document.body.childNodes[1].childNodes[1].textContent);
+// console.log(document.body.childNodes[1].childNodes[1].textContent);
 
-// 2 - selecionando por tag
-const listItens = document.getElementsByTagName("li");
+// // 2 - selecionando por tag
+// const listItens = document.getElementsByTagName("li");
 
-console.log(listItens);
+// console.log(listItens);
 
-// 3 - selecionando por id
+// // 3 - selecionando por id
 
-const selectID = document.getElementById("title");
+// const selectID = document.getElementById("title");
 
-console.log(selectID);
+// console.log(selectID);
 
-// 4 - selecionando por classe
+// // 4 - selecionando por classe
 
-const selectClass = document.getElementsByClassName("product");
+// const selectClass = document.getElementsByClassName("product");
 
-console.log(selectClass);
+// console.log(selectClass);
 
 // 5 - selecionando os elementos por css
 
@@ -35,37 +35,63 @@ console.log(mainContainer);
 
 // 6 - insertBefore
 
-const p = document.createElement("p");
+// const p = document.createElement("p");
 
-const header = selectID.parentElement
+// const header = selectID.parentElement
 
-console.log(header);
+// console.log(header);
 
-header.insertBefore(p, selectID)
+// header.insertBefore(p, selectID)
 
-// 7 - appendChild
-const navLinks = document.querySelector("nav ul");
+// // 7 - appendChild
+// const navLinks = document.querySelector("nav ul");
 
-const li = document.createElement("li");
+// const li = document.createElement("li");
 
-navLinks.appendChild(li);
+// navLinks.appendChild(li);
 
-// 8 - replaceChild
-const h5 = document.createElement("h5");
+// // 8 - replaceChild
+// const h5 = document.createElement("h5");
 
-h5.textContent = "Meu Novo Titulo!";
+// h5.textContent = "Meu Novo Titulo!";
 
-header.replaceChild(h5, selectID);
+// header.replaceChild(h5, selectID);
 
-// 9 - createTextNode
-const myText = document.createTextNode("Agora vamos colocar mais um titulo");
+// // 9 - createTextNode
+// const myText = document.createTextNode("Agora vamos colocar mais um titulo");
 
-console.log(myText);
+// console.log(myText);
 
-const h3 = document.createElement("h3");
+// const h3 = document.createElement("h3");
 
-h3.appendChild(myText);
+// h3.appendChild(myText);
 
-console.log(h3);
+// console.log(h3);
 
-mainContainer.appendChild(h3);
+// mainContainer.appendChild(h3);
+
+// // 10 - trabalhando com atributos 
+
+// const firstLink = navLinks.querySelector("a")
+
+// console.log(firstLink);
+
+// firstLink.setAttribute("href", "https://www.google.com");
+
+// console.log(firstLink.getAttribute("href"));
+
+// firstLink.setAttribute("target", "_blank");
+
+// // 11 - altura e largura 
+// const footer = document.querySelector("footer");
+
+// console.log(footer.offsetWidth);
+// console.log(footer.offsetHeight);
+
+// console.log(footer.clientHeight);
+// console.log(footer.clientWidth);
+
+// 12 - posicao do elemento
+const product1 = productsQuery[0]
+
+console.log(product1.getBoundingClientRect());
