2eb4d7e063f8b8c3cfa2b3d58274a5ebdd0294f8
GUSTAVO DE SOUZA <gustavo_souza12@estudante.sesisenai.org.br>
2024-09-20 21:18:23 -0300

    save

--- SECAO-10-JS/js/scripts.js
+++ SECAO-10-JS/js/scripts.js
@@ -32,3 +32,84 @@ myTitle.addEventListener("click", (event) => {
     console.log(event.pointerType);
     console.log(event.target);
 });
+
+
+
+
+// 6 - eventos de tecla
+document.addEventListener("keyup",(e) =>{
+    console.log(`soltou a tecla ${e.key}`);
+})
+
+document.addEventListener("keydown",(e) =>{
+    console.log(`Segurou a tecla ${e.key}`);
+})
+
+// 7 - eventos de mouse 
+const mouseEvents = document.querySelector("#mouse")
+
+mouseEvents.addEventListener("mousedown", () =>{
+    console.log("Pressionou o Botão");
+})
+
+mouseEvents.addEventListener("mouseup", () =>{
+    console.log("Soltou o Botão");
+})
+
+mouseEvents.addEventListener("dblclick", () =>{
+    console.log("Double Click no Botão");
+})
+
+// 8 - movimento do mouse
+// document.addEventListener("mousemove", (e) => {
+//     console.log(`No eixo X: ${e.x}`);
+//     console.log(`No eixo Y: ${e.y}`);
+// })
+
+// 9 - evento de scroll
+
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200){
+         console.log("Passamos de 200px");
+    }
+})
+
+// 10 - evento de focus/blur
+
+const input = document.querySelector("#my-input")
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no Input");
+})
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu no Input");
+})
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+    console.log("A pagina carregou!");
+})
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+    e.returnValue = "sla";
+})
+
+// 12 - debunce
+const debounce = (f, delay) => {
+    let timeout;
+    return (...arguments) => {
+        if(timeout) {
+            clearInterval(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        },delay)
+    }
+}
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Executando a cada 400ms");
+},400))
